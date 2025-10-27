e660294732a034f3d99955b82f2d33808a3146e2
Jonathan Calugas <calugas.jonathan.18@gmail.com>
2025-10-27 20:52:32 +0800

    commit to save

--- mobile/IcloneReact/domain/llm/deepSeek/model.ts
+++ mobile/IcloneReact/domain/llm/deepSeek/model.ts
@@ -1,3 +1,40 @@
-class DeepSeekModel {
-  baseUrl:string = 'https://api.deepseek.com/chat/completions';
+import axios from 'axios';
+class DeepSeekClient {
+  private readonly apiKey: String; 
+  private readonly baseUrl:string = 'https://api.deepseek.com/chat/completions';
+  private readonly model = 'deepseek-chat';
+
+  constructor (apiKey: string){
+    this.apiKey = apiKey;
+  }
+
+  constructPrompt(): String{
+    return "Prompt";
+  } 
+
+  public async call(content:String):Promise<string> {
+    try{
+      const headers = {
+        'Content-Type': "application/json",
+        Authorization: `Bearer ${this.apiKey}`,
+      }
+
+      const body = {
+        "model":this.model,
+        messages: content,
+        stream: false
+
+      }
+      const response = await axios.post(
+        this.baseUrl,
+        body,
+        {headers}
+      )
+      return response.data;
+    }catch(err: any){
+    }
+    return "response";
+  }
 }
+
+export default DeepSeekClient;
