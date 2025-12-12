e1a1bda5b59810a20a57d337a76e9033e09d46de
Msi <calugas.jonathan.18@gmail.com>
2025-12-12 13:43:41 +0800

    updated the tag and chattag schema to include icon library and icon names

--- mobile/iclone-mobile/data/database/models/tagModel.ts
+++ mobile/iclone-mobile/data/database/models/tagModel.ts
@@ -5,6 +5,8 @@ export class TagModel extends Model {
   static table ='tags';
 
   @field('name') name!: string;
+  @field('icon_library') iconLibrary?: string | null
+  @field('icon_name') iconName?: string | null
 
   @children('chat_tags') chatTags!: any;
 }
--- mobile/iclone-mobile/data/database/schemas/tagSchema.ts
+++ mobile/iclone-mobile/data/database/schemas/tagSchema.ts
@@ -3,7 +3,9 @@ import { tableSchema } from "@nozbe/watermelondb";
 const tagSchema = tableSchema({
   name: 'tags',
   columns: [
-    {name: 'name', type: 'string'}
+    {name: 'name', type: 'string', isOptional: false},
+    {name: 'icon_library', type: 'string', isOptional: true},
+    {name: 'icon_name', type: 'string', isOptional: true}
   ]
 });
 
